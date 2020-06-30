a73209ed746a717896d05d1207d9aea08000cb2c
Habibi Ayoub <habibiayoub87@gmail.com>
2020-06-30 14:19:15 +0100

    list owner. closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 	
-	@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-	public String listOwners() {
+	@Autowired
+	private OwnerService ownerService;
+	
+	@RequestMapping({"", "/", "/index", "/index.html"})
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
 		
 		return "owners/index";
 	}
