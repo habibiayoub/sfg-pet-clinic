2b83c6ad784b2d7af4d88b13161c2826232f4555
Habibi Ayoub <habibiayoub87@gmail.com>
2020-08-12 13:19:09 +0100

    closes #40

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+	
+	@Autowired
+	private PetRepository petRepository;
+
+	@Override
+	public Set<Pet> findAll() {
+		Set<Pet> pets = new HashSet<>();
+		petRepository.findAll().forEach(pets::add);
+		return pets;
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		Optional<Pet> optionalPet = petRepository.findById(id);
+		if (optionalPet.isPresent())
+			return optionalPet.get();
+		else
+			return null;
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return petRepository.save(object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		petRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petRepository.deleteById(id);
+	}
+
+}
