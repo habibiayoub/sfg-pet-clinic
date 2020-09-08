03fda963418adb2d0a9f389c7e6e5879e8efb2b1
Habibi Ayoub <habibiayoub87@gmail.com>
2020-09-08 13:43:55 +0100

    closes #59

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Controller
@@ -19,6 +24,12 @@ public class VetController {
 		
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		
+		return vetService.findAll();
 	}
 
 }
